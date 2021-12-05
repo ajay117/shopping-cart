ade9e2f2d1cff1f716a3a7247245db96390d9a72
Ajay KC <meajay64@gmail.com>
2021-12-05 20:07:38 +0900

    Added basic element to component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,22 @@ import { Link } from "react-router-dom";
 function Home() {
     return (
         <div>
-            <nav>
-                <Link to="/shop">Shop</Link>
-                <Link to="/cart">Cart</Link>
-            </nav>
+            <header className="container-nav">
+                <nav>
+                  <ul>
+                    <li className="brand">BrandName</li>
+                    <li><Link to="/shop">Shop</Link></li>
+                    <li><Link to="/cart">Cart</Link></li>
+                  </ul>
+                </nav>
+            </header>
+            <section>
+                <div className="container-intro">
+                    <p>Welcom to ... </p>
+                    <p>A one stop for all your ... need</p>
+                </div>
+            </section>
+            
         </div>
     )
 }
