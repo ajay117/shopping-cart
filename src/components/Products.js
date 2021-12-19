2e5680d071c6205b4c7f84550260ecac353160c7
Ajay KC <meajay64@gmail.com>
2021-12-19 21:08:49 +0900

    Product will be fetched and will be added to Dom when component mounts

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,21 +1,34 @@
 import { useState, useEffect } from "react";
 
 function Products() {
-  const [product, setProduct] = useState([]);
+  const [products, setProducts] = useState([]);
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products/1')
+    fetch('https://fakestoreapi.com/products')
             .then(res=> res.json())
-            .then(json => {
-              setProduct(json);
-            });
+            .then(json => setProducts([...products, ...json]));
+            
+    return () => {
+      setProducts([]);
+    }
   }, []);
-
+  let addProducts = products.map(item => (
+        <div className="product mh-20 mt-20" key={item.id}>
+          <div className="product-img">
+            <img className="img-responsive" src={item.image} alt={item.title} />
+          </div>
+          <div className="product-description">
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <p className="price">Price: ${item.price}</p>
+            <div className="product-btn">
+              <button className="btn-buy">Buy</button>
+            </div>
+          </div>
+        </div>
+  ));
   return (
     <div>
-      {/* <form>
-        <input type="search" placeholder="Search..."></input>
-      </form> */}
-      
+      {addProducts}
     </div>
   );
 }
